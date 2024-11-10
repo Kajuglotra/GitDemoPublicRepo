5f0cd1007549af134f7726b68f7c708a17606acb
kajal <glotrakajal02@gmail.com>
2024-11-10 11:24:03 +0530

    main changes

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("my first statement");
-        System.out.println("my second statement");   
+        System.out.println("my second statement");
+        System.out.println("my third statement");
+        System.out.println("my fourth statement");
     }
 }
